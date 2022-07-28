e95ddeaf8ca7eb35856f584f5723d32b0f29e319
ZehraGormus <101753225+ZehraGormus@users.noreply.github.com>
2022-07-28 20:31:38 +0200

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,6 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hellojenkins from github webhook!"); 
+        System.out.println("Hellojenkins from github webhook!");
     }
 }
